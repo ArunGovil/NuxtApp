387c3586ed0ab5c65ad8c53e11b39fddc7b574d4
ArunGovil <a.arungovil@gmail.com>
2022-03-10 12:40:36 +0530

    initial commit

---
+++ components/BasicComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is a basic component!</h1>
+</template>
+
+<script>
+export default {
+  name: 'BasicComponent'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <BasicComponent/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
